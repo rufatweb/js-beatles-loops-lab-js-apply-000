8bc7dc526afebe9ab7ed77004dbf9118e40290c8
Rufat Aghayev <rufat.web@gmail.com>
2018-08-09 01:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(arrayMus, arrayIns) {
+  const facts = [];
+  for (var i = 0; (i < arrayMus.length && i < arrayIns.length); i++) {
+      facts.push(arrayMus[i] + ' plays' + arrayIns[i]);
+  }
+  return facts;
+}
