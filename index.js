f4c8aed65d8295f0a79903543b77cee4870b3672
Rufat Aghayev <rufat.web@gmail.com>
2018-08-09 02:04:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // add solution here
 function theBeatlesPlay(arrayMus, arrayIns) {
-  const facts = [];
+  const theyPlay = [];
   for (var i = 0; (i < arrayMus.length && i < arrayIns.length); i++) {
-      facts.push(arrayMus[i] + ' plays ' + arrayIns[i]);
+      theyPlay.push(arrayMus[i] + ' plays ' + arrayIns[i]);
+  }
+  return theyPlay;
+}
+
+function johnLennonFacts(array) {
+  const facts = [];
+  while (i < array.length) {
+    facts.push(array[i] + "!!!")
+    i++;
   }
   return facts;
 }
