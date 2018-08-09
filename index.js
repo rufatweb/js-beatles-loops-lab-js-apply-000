863e01c98062b9e2b03f083942da89b4d6c171d0
Rufat Aghayev <rufat.web@gmail.com>
2018-08-09 01:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(arrayMus, arrayIns) {
   const facts = [];
   for (var i = 0; (i < arrayMus.length && i < arrayIns.length); i++) {
-      facts.push(arrayMus[i] + ' plays' + arrayIns[i]);
+      facts.push(arrayMus[i] + ' plays ' + arrayIns[i]);
   }
   return facts;
 }
